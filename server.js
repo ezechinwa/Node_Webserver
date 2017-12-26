66ac9e44803c31841c58d9c63cc5319da6daa263
Chinwa Williams <chinwawilliams@gmail.com>
2017-12-26 15:39:27 +0100

    SetUo start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const hbs     = require("hbs");
+const port    = process.env.PORT || 3000;
 var app = express();
+
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -23,6 +25,6 @@ app.get('/about', (request,response)=>{
     });
 });
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
 });
